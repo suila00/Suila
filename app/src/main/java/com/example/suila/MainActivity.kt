bc6d4daa1c1acff632277e5817c50c85033146d6
suila <suilasilva00@gmail.com>
2022-02-24 13:05:58 +0000

    funcionalidade1 branch commit4

--- app/src/main/java/com/example/suila/MainActivity.kt
+++ app/src/main/java/com/example/suila/MainActivity.kt
@@ -16,5 +16,6 @@ class MainActivity : AppCompatActivity() {
         //funcionalidade1 branch commit1
         //funcionalidade1 branch commit2
         //funcionalidade1 branch commit3
+        //funcionalidade1 branch commit4
     }
 }
